ed4428df23c4e707b628948950c4b5dc44b03351
Adrian Gąsiorek <adriangasiorek@MacBook-Pro-Adrian.local>
2022-09-11 08:05:31 +0200

    elooooo

--- src/components/home/AssetOfficialEvent.tsx
+++ src/components/home/AssetOfficialEvent.tsx
@@ -5,6 +5,7 @@ import wrenchIcon from "../../images/wrench.png"
 import sportIcon from "../../images/sport.png"
 import ellipsisIcon from "../../images/ellipsis.png"
 import inspirationIcon from "../../images/inspiration.png"
+import { ImageConfig } from "next/dist/shared/lib/image-config";
 
 
     export type AssetOfficialProps = {
@@ -12,7 +13,8 @@ import inspirationIcon from "../../images/inspiration.png"
         date?: string;
         place?: string;
         description?: string;
-
+        category?: string
+        icon?: any
     }
 
 
@@ -20,14 +22,46 @@ export const AssetOfficialEvent = (props: AssetOfficialProps) =>{
 
     const officcialEvents = trpc.useQuery(["officialEvent.getAll"]);
     // console.log(places?.data[0]?.address)
-    
+    console.log(props.category)
     // console.log(props.organizer, props.date)
 
     return(
-        <div className="flex flex-col items-end justify-end font-poppins-bold text-gray-100 border-none  w-[22rem] h-[30rem] bg-copy border border-card-cap-bg mr-5 ml-5 rounded-lg cursor-pointer font-poppins">
-        <div className="bg-copy h-[60%] w-[100%] flex items-center justify-center rounded-lg">
-                <div className="flex justify-center items-center rounded-full h-32 w-32 bg-gray-800">
-                    <Image width={80} height={80} src={concertIcon} ></Image>
+        // <div className="flex flex-col items-end justify-end font-poppins-bold text-gray-100 border-none  w-[22rem] h-[30rem] bg-copy border border-card-cap-bg mr-5 ml-5 rounded-lg cursor-pointer font-poppins">
+
+                <div className={`${props.category== 'Warsztaty' ? 'flex flex-col items-end justify-end font-poppins-bold text-gray-100 border-none  w-[22rem] h-[30rem] bg-copy border border-card-cap-bg mr-5 ml-5 rounded-lg cursor-pointer font-poppins' : 
+                                    props.category=='Sport' ? 'flex flex-col items-end justify-end font-poppins-bold text-gray-100 border-none  w-[22rem] h-[30rem] bg-blu border border-card-cap-bg mr-5 ml-5 rounded-lg cursor-pointer font-poppins':
+                                    props.category=='Koncert' ? 'flex flex-col items-end justify-end font-poppins-bold text-gray-100 border-none  w-[22rem] h-[30rem] bg-concert border border-card-cap-bg mr-5 ml-5 rounded-lg cursor-pointer font-poppins':
+                                    props.category=='Edukacja' ? 'flex flex-col items-end justify-end font-poppins-bold text-gray-100 border-none  w-[22rem] h-[30rem] bg-orange border border-card-cap-bg mr-5 ml-5 rounded-lg cursor-pointer font-poppins':
+                                    props.category=='Inne' ? 'flex flex-col items-end justify-end font-poppins-bold text-gray-100 border-none  w-[22rem] h-[30rem] bg-green border border-card-cap-bg mr-5 ml-5 rounded-lg cursor-pointer font-poppins':
+                                        'flex flex-col items-end justify-end font-poppins-bold text-gray-100 border-none  w-[22rem] h-[30rem] bg-green border border-card-cap-bg mr-5 ml-5 rounded-lg cursor-pointer font-poppins'
+                }`}>
+
+
+
+        {/* <div className="bg-copy h-[60%] w-[100%] flex items-center justify-center rounded-lg"> */}
+        
+                <div className={`${props.category== 'Warsztaty' ? 'bg-copy h-[60%] w-[100%] flex items-center justify-center rounded-lg' : 
+                                    props.category=='Sport' ? 'bg-blu h-[60%] w-[100%] flex items-center justify-center rounded-lg':
+                                    props.category=='Koncert' ? 'bg-concert h-[60%] w-[100%] flex items-center justify-center rounded-lg':
+                                    props.category=='Edukacja' ? 'bg-orange h-[60%] w-[100%] flex items-center justify-center rounded-lg':
+                                    props.category=='Inne' ? 'bg-green h-[60%] w-[100%] flex items-center justify-center rounded-lg':
+                                        'bg-green h-[60%] w-[100%] flex items-center justify-center rounded-lg'
+                }`}>
+                <div className="flex flex-col justify-center items-center rounded-full h-32 w-32 bg-gray-800">
+                    
+                    <div>
+                        {
+                        (props.category== 'Warsztaty') ? <Image width={80} height={80} src={wrenchIcon} ></Image> :
+                        (props.category== 'Sport') ?<Image width={80} height={80} src={sportIcon} ></Image>:
+                        (props.category=='Koncert') ?<Image width={80} height={80} src={concertIcon} ></Image> :
+                        (props.category== 'Edukacja') ? <Image width={80} height={80} src={inspirationIcon} ></Image> :
+                        (props.category== 'Inne') ?<Image width={80} height={80} src={ellipsisIcon} ></Image> :<Image width={80} height={80} src={ellipsisIcon} ></Image>
+                        }
+                    </div>
+                    <div className="absolute mt-48 text-gray-700">{props.category}</div>
+                    
+                    
+                    
                     {/* <a href="https://www.flaticon.com/free-icons/concert" title="concert icons">Concert icons created by Freepik - Flaticon</a> */}
                 </div>
               </div>
--- src/components/home/AssetOfficialEvents.tsx
+++ src/components/home/AssetOfficialEvents.tsx
@@ -3,6 +3,12 @@ import { trpc } from "../../utils/trpc";
 import { date } from "zod";
 import { useSession } from "next-auth/react";
 
+import Image from "next/image";
+import concertIcon from "../../images/concert.png"
+import wrenchIcon from "../../images/wrench.png"
+import sportIcon from "../../images/sport.png"
+import ellipsisIcon from "../../images/ellipsis.png"
+import inspirationIcon from "../../images/inspiration.png"
 
 export const AssetOfficialEvents = () =>{
 
@@ -15,7 +21,7 @@ export const AssetOfficialEvents = () =>{
             <div className="bg-color-gray-900 flex mr-5 ml-5 items-around w-[100%]">
                 {officialEvents.data?.map((officialEvent: any) => 
                 
-                    <AssetOfficialEvent date={officialEvent.date} organizer={officialEvent.organizer} place={officialEvent.place} description={officialEvent.description}> </AssetOfficialEvent>
+                    <AssetOfficialEvent date={officialEvent.date} organizer={officialEvent.organizer} place={officialEvent.place} description={officialEvent.description} category={officialEvent.category}> </AssetOfficialEvent>
                 )}
             </div>
     )
